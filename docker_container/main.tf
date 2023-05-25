687467902092c6e1efecbd70e4f5ed59a79d3d46
ian_zhuang <880831ian@gmail.com>
2023-05-25 17:48:55 +0800

    調整設定

--- docker_container/main.tf
+++ docker_container/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
-  }
-}
-
 provider "docker" {}
 
 resource "docker_image" "nginx" {
